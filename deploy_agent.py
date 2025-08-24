c83ff05eef28f4b387f9b306db2a6e26385220e0
uncured7036 <uncured7036@example.com>
2025-08-24 23:03:50 +0800

    Deploy agent to vertex ai

---
+++ deploy_agent.py
@@ -0,0 +1,45 @@
+import vertexai
+import os
+
+PROJECT_ID = os.environ.get('PROJECT_ID')
+LOCATION = os.environ.get('LOCATION')
+AGENT_DISPLAY_NAME = os.environ.get('AGENT_DISPLAY_NAME')
+AGENT_ID = os.environ.get('AGENT_ID')
+STAGING_BUCKET = os.environ.get('BUCKET')
+
+vertexai.init(
+    project=PROJECT_ID,
+    location=LOCATION,
+    staging_bucket=STAGING_BUCKET,
+)
+
+from multi_tool_agent.agent import root_agent
+
+from vertexai.preview import reasoning_engines
+
+app = reasoning_engines.AdkApp(
+    agent=root_agent,
+    enable_tracing=True,
+)
+
+from vertexai import agent_engines
+
+if not AGENT_ID:
+    remote_agent = agent_engines.create(
+        app,
+        requirements=['google-cloud-aiplatform[agent_engines,adk]'],
+        display_name=AGENT_DISPLAY_NAME,
+        extra_packages=[
+            './multi_tool_agent/'
+        ],
+    )
+else:
+    remote_agent = agent_engines.update(
+        resource_name=AGENT_ID,
+        agent_engine=app,
+        requirements=['google-cloud-aiplatform[agent_engines,adk]'],
+        display_name=AGENT_DISPLAY_NAME,
+        extra_packages=[
+            './multi_tool_agent/'
+        ],
+    )
