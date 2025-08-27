b4531b79356c8063c1bd628ee3eb0eaa548d0dd5
uncured7036 <uncured7036@example.com>
2025-08-27 23:33:52 +0800

    Specify package version

--- deploy_agent.py
+++ deploy_agent.py
@@ -33,7 +33,11 @@ from vertexai import agent_engines
 if not AGENT_ID:
     remote_agent = agent_engines.create(
         app,
-        requirements=['google-cloud-aiplatform[agent_engines,adk]'],
+        requirements=[
+            'cloudpickle==3.1.1',
+            'pydantic==2.11.7',
+            'google-cloud-aiplatform[agent_engines,adk]==1.110.0',
+        ],
         display_name=AGENT_DISPLAY_NAME,
         extra_packages=[
             './trip_agent/'
@@ -43,7 +47,11 @@ else:
     remote_agent = agent_engines.update(
         resource_name=AGENT_ID,
         agent_engine=app,
-        requirements=['google-cloud-aiplatform[agent_engines,adk]'],
+        requirements=[
+            'cloudpickle==3.1.1',
+            'pydantic==2.11.7',
+            'google-cloud-aiplatform[agent_engines,adk]==1.110.0',
+        ],
         display_name=AGENT_DISPLAY_NAME,
         extra_packages=[
             './trip_agent/'
