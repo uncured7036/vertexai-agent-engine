2b084ce19bb6692310589c524ccad242a5fe90c2
uncured7036 <oaf8840kado@outlook.com>
2025-09-09 22:17:24 +0800

    Consolidate activities to childActivities

--- trip_agent/agent.py
+++ trip_agent/agent.py
@@ -48,6 +48,7 @@ Instructions:
 - The activity type must be one of the following: sightseeing, restaurant, shopping, accommodation, freeTime, transport, or other.
 - The transportType must be one of the following: train, highSpeedTrain, flight, bus, taxi, bike, walk, car, boat, motorcycle, or other
 - Please ensure that the each activity has the continuous time slot. Use `freeTime` to fill any gaps and prevent empty activity slots.
+- Consolidate activities into childActivities when their lantitude and longitude are the same.
         """,
         tools=[get_detail_by_google_map, get_candidate_places_by_google_map],
     )
